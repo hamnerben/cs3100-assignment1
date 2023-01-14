2c5efb30bff7fd81be6631a6a4a7886cb68b930a
hamnerben <hamnerben@gmail.com>
2023-01-14 16:38:04 -0700

    correctly performs sanity check on tag

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class main {
     public static void main(String[] args) {
 
@@ -6,21 +8,31 @@ public class main {
         // sanity check on arguments
         boolean goodInput = false;
         if(args.length % 2 == 0){
-            goodInput = true;
             boolean matchesTag;
             for(int i = 0; i < args.length; i++){
 
                 if(i % 2 == 0){ // arg should be tag
                     matchesTag = false;
                     for(String tag : tags){ // iterate through possible tags
-                        if(args[i] == tag){
+                        if(Objects.equals(args[i], tag)){
                             matchesTag = true; // the arg is valid
                         }
+                    }
+                    if(matchesTag){
+                        goodInput = true;
                     }
                 }else { // arg should be an integer
                     System.out.print("");
                 }
             }
+        }
+        if(!goodInput || args.length == 0){
+            System.out.print("--- Assign 1 Help ---\n" +
+                    "-fib [n] : Compute the Fibonacci of [n]; valid range [0, 40]\n" +
+                    "-fac [n] : Compute the factorial of [n]; valid range, [0, 2147483647]\n" +
+                    "-e [n] : Compute the value of 'e' using [n] iterations; valid range [1, 2147483647]");
+        }else {
+            System.out.print("Good input"); //todo: delete
         }
     }
 }
